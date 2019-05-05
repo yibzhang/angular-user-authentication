1678bf0079ab5ff537d6194f0d5395bbe8a5c621
yibzhang <zhangyiboturku@gmail.com>
2019-05-05 20:17:39 +0300

    Inlcude services in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from '_services/in-memory-data.service';
+import { InMemoryDataService } from './_services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
