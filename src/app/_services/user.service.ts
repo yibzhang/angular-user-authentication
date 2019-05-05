a7d54ac21b20b3df40b6f5234620f594b2eda3be
yibzhang <zhangyiboturku@gmail.com>
2019-05-05 20:40:22 +0300

    fix user class path in user.service.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { User } from './_models/user';
+import { User } from '../_models/user';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
