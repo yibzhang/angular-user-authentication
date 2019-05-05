775cedf6db1d2dd01f734e09d6f21c9c107f7f1b
yibzhang <zhangyiboturku@gmail.com>
2019-05-05 20:59:07 +0300

    add getUsers method in register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+export class RegisterComponent implements OnInit {
+  private users: User[];
+
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers():void{
+    this.userService.getUsers().subscribe(users => this.users = users);
   }
 
 }
