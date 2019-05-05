78e8fc6c11241bd666e02cbf4a22cdb90298cdb6
yibzhang <zhangyiboturku@gmail.com>
2019-05-05 20:00:27 +0300

    Added login and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes = [
+//  {path: '', component:}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, 
+                  FormsModule,
+                  RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, LoginComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
